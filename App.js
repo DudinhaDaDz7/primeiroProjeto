33e068be3f9c73464ccb4dae0a598f9735fd4678
DudinhaDaDz7 <DudinhaDaDz7>
2024-03-07 10:50:03 -0300

    Rota

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Nomes: ET Bilu e Dudinha</Text>
+      <Text>Nomes: ET Bilu e Dudinha</Text>
+      <Text>Nomes: ET Bilu e Dudinha</Text>
+      <Title/>
+
       <StatusBar style="auto" />
     </View>
   );
